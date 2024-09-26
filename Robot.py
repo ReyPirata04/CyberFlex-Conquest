496b53fed203a07adfa81fda421551f5752b7799
Lucas Flores <gian.flores@davinci.edu.ar>
2024-09-26 12:06:24 -0300

    Clase Robot Agregada

---
+++ Robot.py
@@ -0,0 +1,12 @@
+class Robot:
+    # Constructor de la clase
+    def __init__(self, nombre, altura, vida, ataque):
+        self.nombre = nombre
+        self.altura = altura
+        self.vida = vida
+        self.ataque = ataque
+
+    #Funciones...
+
+
+
